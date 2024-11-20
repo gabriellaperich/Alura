6d557ddcdac7bdf907c5f08a762436f56ddc096d
Bibi Perich <gabriellaperichguimaraes@gmail.com>
2024-11-20 17:14:07 -0300

    limpando e projetando

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
+import routes from './src/routes/postsRoutes.js';
 
-// inicia o express
-const app = express();
-// .listen(porta, função)
+const app = express(); // inicia o express
+routes(app);
+
 app.listen(3000, () => {
     console.log('Servidor executando...');
-});
-
-app.get('/api', (req, res) => {
-    res.status(200).send('Boas vindas!');
 });
